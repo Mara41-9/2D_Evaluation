0f186597e948f2db47efe6ce9cb3b18c309400af
Nara <dlskfk72@gmail.com>
2026-05-14 17:15:44 +0900

    [다이얼로그 UI] DialogueUI 컴포넌트 생성 + 프리팹에 추가

---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUI : UIBase
+{
+    [SerializeField] private Text Text_Description;
+    public string _currentDialogueId;
+
+    public void StartDialogue(string dialogueId)
+    {
+        var dialogueData = GameDataManager.Instance.GetDialogueData(dialogueId);
+        if(dialogueData == null)
+        {
+            Debug.LogWarning($"다이얼로그 데이터가 존재하지 않습니다 {dialogueData}");
+            return;
+        }
+
+        _currentDialogueId = dialogueId;
+    }
+}
