993048248f5ade18a52a4d1c2ddb2c277aa4b2b4
Nara <dlskfk72@gmail.com>
2026-05-14 22:26:05 +0900

    [카메라 이동] 플레이어의 움직임에 따라 카메라가 이동하게 구현

---
+++ Assets/Scripts/2D/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("따라갈 플레이어")]
+    [SerializeField] private Transform Prefab_Player;
+
+    [Header("카메라 속도")]
+    [SerializeField] private float _moveSpeed = 5f;
+
+    [Header("카메라 거리")]
+    [SerializeField] private Vector3 _offset = new Vector3(8f, 5f, -10f);
+
+    private void LateUpdate()
+    {
+        StartMove();
+        
+    }
+
+    private void StartMove()
+    {
+        Vector3 targetPos = _offset + Prefab_Player.position;
+
+        this.transform.position = Vector3.Lerp(transform.position, targetPos, _moveSpeed * Time.deltaTime);
+
+
+    }
+}
--- Assets/Scripts/UI/GameTestUI.cs
+++ Assets/Scripts/UI/GameTestUI.cs
@@ -7,45 +7,6 @@ public class GameTestUI : MonoBehaviour
 
     [SerializeField] private SpawnSpot SpawnSpot_Monster;
 
-    private void OnEnable()
-    {
-        // UGUI꺼
-        // Button_AAA.onClick.AddListener(OnClick_DataLoadTest);
-        // 제가만든 거
-        // Button_BBB.BindOnClickButtonEvent(OnClick_DataLoadTest);
-
-        //InputField_ConsoleCommand.onSubmit.AddListener(OnSubmit_Input);
-        //InputField_ConsoleCommand.onValueChanged.AddListener(OnValueChanged_Input);
-    }
-
-    private void OnDisable()
-    {
-        //InputField_ConsoleCommand.onSubmit.RemoveAllListeners();
-        //InputField_ConsoleCommand.onValueChanged.RemoveAllListeners();
-    }
-
-    public void OnSubmit_Input(string str)
-    {
-        //string inputedText = InputField_ConsoleCommand.text;
-        //var characterData = GameDataManager.Instance.GetCharacterData(inputedText);
-        //if (characterData != null)
-        //{
-        //    Debug.LogWarning(characterData.Name);
-        //}
-
-        //Debug.LogWarning(str);
-    }
-
-    public void OnValueChanged_Input(string str)
-    {
-        //Debug.Log(str);
-    }
-
-    public void OnClick_DataLoadTest()
-    {
-        GameDataTester.StartDataTest();
-    }
-
     public void OnClick_SelectTestBtn()
     {
         SpawnSpot_Monster.StartSpawn();
