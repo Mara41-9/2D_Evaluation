3cef795960515387f0f7b2c39d505c3e99d7eb73
Nara <dlskfk72@gmail.com>
2026-05-14 19:07:53 +0900

    [필드오브젝트 데이터드리븐] 필드 오브젝트 컴포넌트 추가 (우선 간단히 데이터Id와 인스턴스 Id를 갖고 있는다)

---
+++ Assets/Scripts/2D/FieldObject2D.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class FieldObject2D : MonoBehaviour
+{
+    [SerializeField] private string _fieldObjectInstanceId;  // 필드 오브젝트 개별 인스턴스 ID
+    [SerializeField] private string _fieldObjectDataId;      // 어떤 종류의 오브젝트인지 ID
+    [SerializeField] private string _fieldObjectName;        // 오브젝트 이름
+
+    // 필드 오브젝트 생성 직후 초기화 함수
+    public void InitFieldObjectInfoOnCreated(int instanceId, string fieldObjectDataId)
+    {
+        // GameDataManager에서 해당 ID의 데이터 가져오기
+        var fieldObjectData = GameDataManager.Instance.GetFieldObjectData(fieldObjectDataId);
+        if(fieldObjectData == null)
+        {
+            Debug.LogWarning($"유효하지 않은 필드 오브젝트 데이터 입니다! {fieldObjectDataId}");
+            return;
+        }
+
+        // 현재 오브젝트 데이터 ID 저장
+        _fieldObjectDataId = fieldObjectDataId;
+    }
+
+    // 현재 오브젝트 데이터 ID 반환 함수
+    public string GetFieldObjectDataId()
+    {
+        return _fieldObjectDataId;
+    }
+
+    // 트리거 충돌 시 호출되는 유니티 이벤트 함수
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") == true)
+        {
+
+        }
+    }
+}
