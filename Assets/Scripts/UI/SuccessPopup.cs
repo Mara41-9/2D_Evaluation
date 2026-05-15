d6543973fb2409230b6ba83ae88a72ddf35ab4f6
Nara <dlskfk72@gmail.com>
2026-05-15 22:47:01 +0900

    [UI] SuccessPopup.cs 로직 생성 + 구현 + 씬 업데이트

---
+++ Assets/Scripts/UI/SuccessPopup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SuccessPopup : MonoBehaviour
+{
+    [SerializeField] private GameUIButton Btn_GoMainUI;
+
+    public void OnEnable()
+    {
+        Btn_GoMainUI.BindOnClickButtonEvent(OnClick_GoMainUIBtn);
+    }
+
+    private void OnClick_GoMainUIBtn()
+    {
+        UIManager.Instance.OpenLobbyMainUI();
+        Debug.LogWarning("메인UI로 돌아갑니다.");
+    }
+}
