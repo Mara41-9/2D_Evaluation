22d282e75985eedecb6ee75d9a8793ba847fa0fa
Nara <dlskfk72@gmail.com>
2026-05-15 12:42:22 +0900

    [Fix] Item 어드레서블 경로 수정 - 엑셀 파일 수정

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryUI : MonoBehaviour
+public class InventoryUI : UIBase
 {
     // 생성할 슬롯 오브젝트
     [SerializeField] private GameObject Prefab_Slot;
