c15e2a202751bbfd4c18090c576715f8e8966844
Nara <dlskfk72@gmail.com>
2026-05-15 19:32:02 +0900

    [버튼] Exit 버튼 클릭 시, UI씬으로 전환되고 MainUI로 이동되게 구현

--- Assets/Scripts/UI/GameTestUI.cs
+++ Assets/Scripts/UI/GameTestUI.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameTestUI : MonoBehaviour
@@ -7,8 +8,22 @@ public class GameTestUI : MonoBehaviour
 
     [SerializeField] private SpawnSpot SpawnSpot_Monster;
 
+    [SerializeField] private GameUIButton Btn_Exit;
+
+    public void OnEnable()
+    {
+        Btn_Exit.BindOnClickButtonEvent(OnClick_ExitButton);
+        
+    }
+
     public void OnClick_SelectTestBtn()
     {
         SpawnSpot_Monster.StartSpawn();
+    }
+
+    private void OnClick_ExitButton()
+    {
+        Debug.LogWarning("UI씬 메인UI로 이동합니다.");
+        SceneManager.LoadScene("UI_Basic");
     }
 }
